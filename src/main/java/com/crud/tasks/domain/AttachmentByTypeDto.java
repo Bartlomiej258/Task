c914677c88129dccd7dceb433f90c3abc89b1472
Bartlomiej Lorenowicz <barteklorenowicz258@gmail.com>
2023-02-05 22:24:49 +0100

    23.3 add mail to project

---
+++ src/main/java/com/crud/tasks/domain/AttachmentByTypeDto.java
@@ -0,0 +1,14 @@
+package com.crud.tasks.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AttachmentByTypeDto {
+    @JsonProperty("trello")
+    private Trello trello;
+}
--- src/main/java/com/crud/tasks/domain/Mail.java
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -1,13 +1,17 @@
 package com.crud.tasks.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
+@Setter
 @Getter
 @Builder
+@AllArgsConstructor
 public class Mail {
-    private final String mailTo;
-    private final String subject;
-    private final String message;
-    private final String toCc;
+    private String mailTo;
+    private String subject;
+    private String message;
+    private String toCc;
 }
